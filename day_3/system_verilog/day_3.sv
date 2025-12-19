9feb392f57939af4b703d7ac7f261433cb46eb50
robertsaabwoo <1robertsolomonsaab@gmail.com>
2025-12-18 22:51:27 -0500

    removed some useless lines and optimized away one whole clock cycle wow

--- day_3/system_verilog/day_3.sv
+++ day_3/system_verilog/day_3.sv
@@ -14,15 +14,9 @@ module day_3 #(
     axi_stream_if.master m_axis
 );
 
-    localparam integer MAXLENGTH = INPUTWIDTH + 4*$ceil(INPUTWIDTH/3);
-    localparam integer DIGITS = (MAXLENGTH % 4 ==0)?MAXLENGTH/4 : MAXLENGTH/4 + 1;
-    localparam integer DIGIT_BITS = DIGITS*4;
-
     logic [OUTPUTWIDTH-1:0] sum;
-    logic [INPUTWIDTH-1:0] input_r, input_n;
+    logic [INPUTWIDTH-1:0] input_r;
     logic [3:0] largest_digit;
-
-    logic first_digit;
 
     logic [3:0] max_digit_1_r, max_digit_1_n, max_digit_2_r, max_digit_2_n;
 
@@ -44,11 +38,11 @@ module day_3 #(
                 S_IDLE: begin
                     m_axis.tvalid <= 0;
                     if (s_axis.tready && s_axis.tvalid) begin
-                        input_r <= s_axis.tdata;
-                        first_digit <= 1;
+                        input_r <= s_axis.tdata/10;
                         state_r <= S_FIND_MAX;
                         max_digit_1_r <= '0;
-                        max_digit_2_r <= '0;
+                        //since we know we dont have to consider any other data for the first digit, we can simply load its value (save a clock cycle)
+                        max_digit_2_r <= s_axis.tdata%10;
                         largest_digit <= '0;
                         state_n <= (s_axis.tlast)? S_OUTPUT : S_IDLE;
                     end
@@ -57,9 +51,8 @@ module day_3 #(
                     automatic logic [3:0] digit = input_r % 10;
                     max_digit_1_n = max_digit_1_r;
                     max_digit_2_n = max_digit_2_r;
-                    if (first_digit)
-                        max_digit_2_n = digit;
-                    else if (digit >= max_digit_1_n && first_digit != 1) begin
+
+                    if (digit >= max_digit_1_n) begin
                         max_digit_1_n = digit;
                         max_digit_2_n = largest_digit; // at this point the largest digit is behind the current
                     end
@@ -69,7 +62,6 @@ module day_3 #(
                         sum <= sum + max_digit_1_r*10+ max_digit_2_r;
                     end
                     input_r <= input_r/10;
-                    first_digit <= 0;
                     max_digit_1_r <= max_digit_1_n;
                     max_digit_2_r <= max_digit_2_n;
                     largest_digit <= (digit>largest_digit)? digit:largest_digit;
